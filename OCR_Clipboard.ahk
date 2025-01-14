a8405a753db68133cb76f36020e18d3a7916d639
Sheschire <theo.lemesle42@gmail.com>
2025-01-14 15:10:42 +0100

    Add OCR_Clipboard

---
+++ OCR_Clipboard.ahk
@@ -0,0 +1,14 @@
+﻿#Requires AutoHotkey v2.0
+
+!`:: ;
+{
+    FolderPath := <your path of the project>
+
+    RunWait("SnippingTool /clip")
+    Sleep(500)
+
+    PythonPath := <your path to python>
+    RunWait(PythonPath . " " . FolderPath . "OCR_Clipboard.py")
+
+    return
+}
---
+++ OCR_Clipboard.py
@@ -0,0 +1,15 @@
+from pytesseract import pytesseract
+from PIL import ImageGrab
+import pyperclip
+
+# Configure Tesseract path
+pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+image = ImageGrab.grabclipboard()
+image = image.convert('L')
+
+# Extract text
+text = pytesseract.image_to_string(image)
+pyperclip.copy(text)
+
+image = ImageGrab.grabclipboard()
